e18c1152ad0801cc28edc46afb0339a5424893d1
Veronika <nika.kripen@gmail.com>
2016-07-05 12:27:11 +0300

    add singleton example

---
+++ Singleton/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Singleton
+{ 
+    class MainApp
+    {
+        static void Main()
+        {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            // Same instance?
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            // Load balance 15 server requests
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+
+           Console.ReadKey();
+        }
+    }
+
+    class LoadBalancer
+    {
+        private static LoadBalancer _instance;
+        private List<string> _servers = new List<string>();
+        private Random _random = new Random();
+
+        // Lock synchronization object
+        private static object syncLock = new object();
+
+        // Constructor (protected)
+        protected LoadBalancer()
+        {
+            // List of available servers
+            _servers.Add("ServerI");
+            _servers.Add("ServerII");
+            _servers.Add("ServerIII");
+            _servers.Add("ServerIV");
+            _servers.Add("ServerV");
+        }
+
+        public static LoadBalancer GetLoadBalancer()
+        {
+            if (_instance == null)
+            {
+                lock (syncLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new LoadBalancer();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        public string Server
+        {
+            get
+            {
+                int r = _random.Next(_servers.Count);
+                return _servers[r];
+            }
+        }
+    }
+}
